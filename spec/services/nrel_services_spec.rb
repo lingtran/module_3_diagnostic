3da10158ca4041b6e202d85767341f43cf99a3a8
lingtran <lxiutran@gmail.com>
2016-06-06 10:38:25 -0600

    modify get_nearest_stations NREL service method

--- spec/services/nrel_services_spec.rb
+++ spec/services/nrel_services_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "NREL services" do
   VCR.use_cassette "nrel_alt_fuels" do
     service = NrelServices.new
     zipcode = "80203"
-    response = service.get_nearest_stations(zipcode)
+    raw_response = service.get_nearest_stations(zipcode)
+    parsed_response = service.parse_nearest_stations(raw_response)
   end
 end
